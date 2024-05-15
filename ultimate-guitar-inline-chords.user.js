8b8882b81921447d3c4779a69fd5511d75d2c946
Ricardo Moura <ricardo.sanfinsmoura@gmail.com>
2024-05-15 17:48:58 +0100

    Add version and update/download URLs

--- ultimate-guitar-inline-chords.user.js
+++ ultimate-guitar-inline-chords.user.js
@@ -7,6 +7,9 @@
 // @match        https://tabs.ultimate-guitar.com/tab/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ultimate-guitar.com
 // @grant        GM_addStyle
+// @version      0.1.0
+// @updateURL    https://github.com/RMoura98/Ultimate-Guitar-inline-chords/raw/main/ultimate-guitar-inline-chords.user.js
+// @downloadURL  https://github.com/RMoura98/Ultimate-Guitar-inline-chords/raw/main/ultimate-guitar-inline-chords.user.js
 // ==/UserScript==
 
 GM_addStyle("span[data-name] {font-size: 0; margin: -25px 0px !important; display: inline-block; height: 50px; transform: translate(0px, 15px);} span[data-name]:after {content: none;}")
